1f0e6a05e11ea53456b55b1a2b32257bc59bcd16
Lynn <code@krogseng.com>
2017-02-05 23:24:41 -0800

    Added  greeting, cowsay, and fact GET.

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -13,7 +13,7 @@ describe('testing http server with chai-http', () => {
 
     const request = chai.request(server);
 
-    it('GET / says "hello <name>" ', done => {
+    it('GET / says "hello stranger" ', done => {
         request
             .get('/greeting')
             .end((err, res) => {
@@ -22,5 +22,41 @@ describe('testing http server with chai-http', () => {
             });
     });
 
+    it('GET / says "hello <name>" ', done => {
+        request
+            .get('/greeting/lynn')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'hello lynn');
+                done();
+            });
+    });
 
-});
+    it('GET / says "<salutation> <name>', done => {
+        request
+            .get('/greeting/lynn?salutation=bonjour')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'bonjour lynn');
+                done();
+            })
+    });
+
+    it('GET / fact', done => {
+        request
+            .get('/fact')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'Development of HTTP was initiated by Tim Berners-Lee at CERN in 1989');
+                done();
+            });
+    });
+
+    it('POST / says "Cannot POST ', done => {
+        request
+            .post('/')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'Cannot POST ');
+                assert.strictEqual(res.statusCode, 404);
+                done();
+            });
+    });
+
+}); // describe
