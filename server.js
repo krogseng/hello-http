9eae18871b7f049c7faf986a55399eb6a3d65fc4
Lynn <code@krogseng.com>
2017-02-22 09:54:12 -0800

    changed PORT to process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // get the server started
 const server = require('../lib/http-server');
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening on: ', PORT);
 });
